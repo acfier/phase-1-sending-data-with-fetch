2451f1f84141a2d44d3a7d3f8d075386de9130fb
acfier <acfier@gmail.com>
2022-04-22 00:48:50 -0400

    commit tree e525e124658baf03a0ae1790fcb364f092392188

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+
+
+function submitData(userName, email) {
+    const newObj = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            'name': userName,
+            'email': email
+        })
+    };
+    return fetch('http://localhost:3000/users', newObj)
+    .then(resp => resp.json())
+    .then(body => addNewContact(body.id))
+
+    .catch(response => 'Unauthorized Access')
+}
+
+    function addNewContact(newID) {
+        document.getElementsByTagName('body')[0].innerHTML = newID;
+        document.body.append('Unauthorized Access')
+    }
+
+    submitData('Name', 'name@name.com')
